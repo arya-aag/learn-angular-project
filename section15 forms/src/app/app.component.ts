f80b03fe724fdeab608e5cd1488c4768173c7005
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-07-28 19:02:39 +0530

    two way binding

--- section15 forms/src/app/app.component.ts
+++ section15 forms/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
