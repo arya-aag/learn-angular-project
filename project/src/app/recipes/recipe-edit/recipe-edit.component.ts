73aa93c9b636c556bbe189f7633cc3e24de06eda
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-07-30 08:26:58 +0530

    add initial reactive form for recipe edit

--- project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,17 +10,22 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-          this.editMode = params['id'] != null;
-        }
-      );
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
   }
 
+  private initForm() {
+    this.recipeForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+  }
 }
