46fbabc5e0ee988100ee923dd1e3a37927be5816
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-08-01 01:31:45 +0530

    create basic signup func

--- project/src/app/auth/auth.service.ts
+++ project/src/app/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +8,11 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   constructor() {}
 
-  signupUser(email: string, password: string) {}
+  signupUser(email: string, password: string) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(console.log)
+      .catch(console.log);
+  }
 }
