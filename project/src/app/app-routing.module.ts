cc280cc564317b477f54c512fc07122122113ee0
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-08-07 09:10:08 +0530

    add canLoad guard to recipes route

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
--- project/src/app/auth/auth.guard.ts
+++ project/src/app/auth/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanLoad, Route } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authSrv: AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.authSrv.isAuthenticated();
+  }
+
+  canLoad(route: Route): boolean {
     return this.authSrv.isAuthenticated();
   }
 }
