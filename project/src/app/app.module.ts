fd5b8e25d5f4ad62ac160e1f54d08c84d33fda7d
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-08-07 08:53:17 +0530

    add auth module with its own routes

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,23 +3,21 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStoreService } from './shared/data-store.service';
-import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SignupComponent, SigninComponent],
-  imports: [BrowserModule, HttpModule, FormsModule, RecipesModule, AppRoutingModule, SharedModule, ShoppingListModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, HttpModule, RecipesModule, AppRoutingModule, SharedModule, ShoppingListModule, AuthModule],
   providers: [ShoppingListService, RecipeService, DataStoreService],
   bootstrap: [AppComponent]
 })
