5562ccdc64c11044f8cd80a11b725669c5f622f3
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-08-16 02:24:27 +0530

    undo wrong commit

--- project/src/app/recipes/store/recipes.reducers.ts
+++ project/src/app/recipes/store/recipes.reducers.ts
@@ -55,7 +55,6 @@ export function recipeReducer(state = initialState, action: RecipeActs.RecipeAct
         recipes: [...recipeList]
       };
 
-      AbstractFormGroupDirective;
     case RecipeActs.DELETE_RECIPE:
       const recipesList = state.recipes;
       recipesList.splice(action.payload, 1);
